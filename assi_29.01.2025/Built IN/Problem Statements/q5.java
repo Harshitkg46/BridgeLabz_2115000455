b98ccd6572a66bca06064c9307f05b93aa42a467
Harshit Goyal <harshitkg2003@gmail.com>
2025-01-29 23:41:11 +0530

    Problem Statements

---
+++ assi_29.01.2025/Built IN/Problem Statements/q5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class q5 {
+   // TODO Auto-generated method stub
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       System.out.println(isPalindrome(scanner.next()) ? "Palindrome" : "Not Palindrome");
+       scanner.close();
+   }
+   static boolean isPalindrome(String s) {
+       int l = 0, r = s.length() - 1;
+       while (l < r) if (s.charAt(l++) != s.charAt(r--)) return false;
+       return true;
+   }
+}
